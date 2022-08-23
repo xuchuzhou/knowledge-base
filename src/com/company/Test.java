3e241d30319a1c32213fc7e1b2c453b395f0538f
lueluelue <1799659934@qq.com>
2022-08-23 21:01:03 +0800

    2-1 测试

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public static void main(String[] args) {
        Solustion s =new Solustion();
         System.out.println(s.isPalindrome(121));
-
+        System.out.println(s.isPalindrome(12111));
 
     }
 
